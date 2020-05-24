1e9bff3f4c096a426a2b435e0f6a10e73499515c
Manoj Kumar Manivannan <manoemails@gmail.com>
2020-05-25 00:09:07 +0530

    upgraded to spring boot version 2.3.0

--- src/main/java/com/cluster/corp/config/server/config/WebSecurityConfiguration.java
+++ src/main/java/com/cluster/corp/config/server/config/WebSecurityConfiguration.java
@@ -1,7 +1,5 @@
 package com.cluster.corp.config.server.config;
 
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,7 +9,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
-				.requestMatchers(EndpointRequest.toAnyEndpoint()).authenticated().and().httpBasic();
+		http.csrf().disable().authorizeRequests().anyRequest().permitAll().and().httpBasic();
 	}
 }
